2aa7e03af54029f9ef37c24a831301a690a57964
Ayamainstream <201692@astanait.edu.kz>
2021-10-11 00:22:44 +0600

    commit

--- src/coinmap.py
+++ src/coinmap.py
@@ -1,5 +1,7 @@
 from requests import Request, Session, get
 import json
+from bs4 import BeautifulSoup
+import requests
 
 class coin:
     
@@ -13,21 +15,23 @@ class coin:
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
         }
         id = self.get_id(coin_name)
-        url = f"https://api.coinmarketcap.com/content/v3/news?coins={id}&page=1&size=15"
+        url = f"https://api.coinmarketcap.com/content/v3/news?coins={id}&page=1&size=12"
         request = get(url, headers)
         text = request.json()
         data = text['data']
         coins_news = []
-        for n in data:
-            if n['meta']:
-                meta = n['meta']
+        for news in data:
+            if news['meta']:
+                meta = news['meta']
                 coin_news = []
                 coin_news.append(meta['title'])
-                coin_news.append(meta['subtitle'])
-                coin_news.append(meta['sourceUrl'])
+                html = get(meta['sourceUrl'], headers=headers).text
+                soup = BeautifulSoup(html, "lxml") 
+                for p in soup.find_all('p'): 
+                    coin_news.append(p.get_text(" ", strip=True))
                 coins_news.append(coin_news)
         print(coins_news)
-
+        
 
     def get_id(self, coin_name):
         headers = {
